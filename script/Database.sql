1e4da95b2d716872641b49b575729aade2f90e28
Romário <romarioroberto@gmail.com>
2026-03-05 09:19:19 -0400

    Adicionado tabela usuarios

--- script/Database.sql
+++ script/Database.sql
@@ -3,3 +3,28 @@ CREATE DATABASE IF NOT EXISTS matchvagas;
 
 # Seleciona a database matchvagas
 USE matchvagas;
+
+CREATE TABLE IF NOT EXISTS usuarios(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    dataNascimento DATE,
+    idade INT,
+    telefone INT,
+    ativo BOOLEAN,
+    dataCadastro DATE,
+    dataUltimoAcesso DATE,
+    FOREIGN KEY (telefone_id) REFERENCES telefones(id)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS notificacoes(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    titulo varchar(255),
+    mensagem TEXT,
+    tipo enum('NOVA_VAGA','STATUS_CANDIDATURA',
+        'MENSAGEM_EMPRESA','ALERTA_SISTEMA','VAGA_EXPIRADA'),
+    dataEnvio date,
+    lida bool,
+    usuario_id int not null ,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
